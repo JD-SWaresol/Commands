45836d41b9685d45f1ac91d081eda0714c0c0f85
JD-SWaresol <juan.delgado@smartwaresol.com>
2025-04-01 11:51:27 -0600

    NextJs/

---
+++ NextJs/Commands.sh
@@ -0,0 +1,16 @@
+"#############################################################################################"
+"####################################### NEXTJS - COMMANDS ###################################"
+"#############################################################################################" 
+
+### Comandos NextJS
+
+-----> Crear una aplicación desde cero instalando la version mas nueva de NextJS
+
+~$ npx create-next-app@latest
+
+~$ npx create-next-app@latest --typescript	--=> En caso de que necesitemos usar TypeScript dentro del proyecto
+
+
+-----> Instalar una libreria de React para Iconos dentro de un Proyecto Node JS
+
+~$ npm install react-icons
